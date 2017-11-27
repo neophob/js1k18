c1548f6066a31977f482d2f9d9890bbd24472ef5
michuNEEO <michael@neeo.com>
2017-11-27 13:59:03 +0100

    requestanimationframe is already provided 1244

--- js1k.js
+++ js1k.js
@@ -26,7 +26,7 @@ var heightmap, colormap; // 1024*1024 byte array with height information
 // ---------------------------------------------
 // Screen data
 
-var buf8, buf32, imagedata = c.createImageData(a.width, a.height), context, time=0;
+var buf8, buf32, imagedata = c.createImageData(a.width, a.height), time=0;
 
 //var pallete = [0xff000000, 0xff000099,  0xff000000];// 0xff0000ff, 0xffFFD38C];
 var pallete = [0x000ff0, 0x113231, 0x2d616e, 0xFFD38C];
@@ -143,9 +143,7 @@ var Draw = () => {
 
     // Flip, Show the back buffer on screen
     imagedata.data.set(buf8);
-    context.putImageData(imagedata, 0, 0);
-
-    window.requestAnimationFrame(Draw);
+    c.putImageData(imagedata, 0, 0);
 }
 
 
@@ -254,6 +252,5 @@ tmp = new ArrayBuffer(a.width * a.height * 4);
 buf8   = new Uint8Array(tmp);
 buf32  = new Uint32Array(tmp);
 
-Draw();
-
+requestAnimationFrame(Draw);
 })();
